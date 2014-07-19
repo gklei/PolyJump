38243cba3fd32d4a50f0fae287266a85731d3a15
David Cheng <davechengapps@gmail.com>
2014-07-19 11:54:50 -0400

    Fixed warning when compiling for device

--- PolyJump/PJAppDelegate.m
+++ PolyJump/PJAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-   srand(time(NULL));
+   srand((unsigned int)time(NULL));
     return YES;
 }
 							
